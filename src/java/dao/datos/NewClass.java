be194a5445b19498044b9f109acde00ea239b88e
Stefvn <>
2018-03-19 10:58:45 +0100

    inicio de capa logica con modelo: caracteristicas incluidos,

--- src/java/dao/datos/NewClass.java
+++ src/java/dao/datos/NewClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao.servicios;
+package dao.datos;
 
 import entidades.Oferente;
 import java.sql.SQLException;
