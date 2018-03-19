6b4efeae63bf50ed60d35eb66af453c454abd427
pc <pc@xmen2>
2018-03-19 11:02:57 +0100

    Revert "inicio de capa logica con modelo: caracteristicas incluidos, "
    This reverts commit be194a5445b19498044b9f109acde00ea239b88e.

--- src/java/dao/servicios/Dao.java
+++ src/java/dao/servicios/Dao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao.datos;
+package dao.servicios;
 
 import entidades.Aplicado;
 import entidades.Caracteristicas;
